1151e628de3f021f243388056a4f45b85e3f1a4d
RandomHashTags <imrandomhashtags@gmail.com>
2024-10-19 03:46:17 -0500

    now supports graceful shutdown

--- Sources/Destiny/Server.swift
+++ Sources/Destiny/Server.swift
@@ -12,14 +12,13 @@ import DestinyUtilities
 
 // MARK: Server
 public actor Server : Service {
+    static var connections:[Int32:Task<(), Swift.Error>] = Dictionary(minimumCapacity: 50)
+
     let address:String?
     let port:in_port_t
     let maxPendingConnections:Int32
-    let logger:Logger
-
     let routers:[Router]
-
-    var connections:[Int32:Task<(), Swift.Error>]
+    let logger:Logger
 
     public init(
         address: String? = nil,
@@ -33,8 +32,6 @@ public actor Server : Service {
         self.maxPendingConnections = maxPendingConnections
         self.routers = routers
         self.logger = logger
-        connections = [:]
-        connections.reserveCapacity(Int(maxPendingConnections))
     }
 
     public func run() async throws {
@@ -44,7 +41,7 @@ public actor Server : Service {
         }
         Socket.noSigPipe(fileDescriptor: fileDescriptor)
         var binded:Int32 = -1
-        var addr = sockaddr_in6(
+        var addr:sockaddr_in6 = sockaddr_in6(
             sin6_len: UInt8(MemoryLayout<sockaddr_in>.stride),
             sin6_family: UInt8(AF_INET6),
             sin6_port: port.bigEndian,
@@ -67,51 +64,60 @@ public actor Server : Service {
             unistd.close(fileDescriptor)
             throw Server.Error.listenFailed()
         }
+        let static_responses:[String:RouteResponseProtocol] = Self.static_responses(for: routers)
+        let not_found_response:StaticString = StaticString("HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length:9\r\n\r\nnot found")
         logger.notice(Logger.Message(stringLiteral: "Listening for clients on port \(port)"))
-        var staticResponses:[String:RouteResponseProtocol] = [:]
-        for router in routers {
-            for (path, responder) in router.staticResponses {
-                staticResponses[String(path)] = responder
-            }
-        }
-        let response:StaticString = StaticString("HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length:9\r\n\r\nnot found")
-        while !Task.isCancelled {
-            do {
-                let clientFileDescription:Int32 = try await client(fileDescriptor: fileDescriptor)
-                let connection:Task<(), Swift.Error> = Task.detached {
-                    let client:Socket = Socket(fileDescriptor: clientFileDescription)
-                    let tokens:[Substring] = try client.readHttpRequest()
-                    if let responder:RouteResponseProtocol = staticResponses[tokens[0] + " " + tokens[1]] {
-                        try await responder.respond(to: consume client)
-                    } else {
-                        var err:Swift.Error? = nil
-                        response.withUTF8Buffer {
-                            do {
-                                try client.write($0.baseAddress!, length: $0.count)
-                            } catch {
-                                err = error
+        await withTaskCancellationOrGracefulShutdownHandler {
+            while true {
+                do {
+                    let client:Int32 = try await Self.client(fileDescriptor: fileDescriptor)
+                    let connection:Task<(), Swift.Error> = Task.detached {
+                        let client_socket:Socket = Socket(fileDescriptor: client)
+                        let tokens:[Substring] = try client_socket.readHttpRequest()
+                        if let responder:RouteResponseProtocol = static_responses[tokens[0] + " " + tokens[1]] {
+                            try await responder.respond(to: consume client_socket)
+                        } else {
+                            var err:Swift.Error? = nil
+                            not_found_response.withUTF8Buffer {
+                                do {
+                                    try client_socket.write($0.baseAddress!, length: $0.count)
+                                } catch {
+                                    err = error
+                                }
+                            }
+                            unistd.close(client)
+                            if let error:Swift.Error = err {
+                                throw error
                             }
                         }
-                        if let error:Swift.Error = err {
-                            throw error
-                        }
+                        Self.connections[client] = nil
                     }
-                    await self.closed(clientFileDescription)
+                    Self.connections[client] = connection
+                } catch {
+                    logger.error(Logger.Message.init(stringLiteral: "\(error)"))
                 }
-                connections[clientFileDescription] = connection
-            } catch {
-                logger.error(Logger.Message.init(stringLiteral: "\(error)"))
+            }
+        } onCancelOrGracefulShutdown: {
+            for (client, connection) in Self.connections {
+                connection.cancel()
+                unistd.close(client)
+            }
+            unistd.close(fileDescriptor)
+        }
+    }
+    @inlinable
+    static func static_responses(for routers: [Router]) -> [String:RouteResponseProtocol] {
+        var responses:[String:RouteResponseProtocol] = [:]
+        responses.reserveCapacity(routers.count)
+        for router in routers {
+            for (path, responder) in router.staticResponses {
+                responses[String(path)] = responder
             }
         }
-        // server has stopped
-        for (fileDescription, connection) in connections {
-            connection.cancel()
-            unistd.close(fileDescription)
-        }
-        unistd.close(fileDescriptor)
+        return responses
     }
 
-    func client(fileDescriptor: Int32) async throws -> Int32 {
+    static func client(fileDescriptor: Int32) async throws -> Int32 {
         return try await withCheckedThrowingContinuation { continuation in
             var addr:sockaddr = sockaddr(), len:socklen_t = 0
             let client:Int32 = accept(fileDescriptor, &addr, &len)
@@ -121,9 +127,6 @@ public actor Server : Service {
             }
             continuation.resume(returning: client)
         }
-    }
-    func closed(_ client: Int32) {
-        self.connections[client] = nil
     }
 }
 // MARK: Server.Error
