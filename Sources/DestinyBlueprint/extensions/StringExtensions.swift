47c75ebfec1264913d615ffe64a842c0063330fe
RandomHashTags <imrandomhashtags@gmail.com>
2025-10-30 01:02:55 -0500

    remove unused function extension for `String` (appending an inline array to a string)

--- Sources/DestinyBlueprint/extensions/StringExtensions.swift
+++ Sources/DestinyBlueprint/extensions/StringExtensions.swift
@@ -5,19 +5,6 @@ extension String {
     #if Inlinable
     @inlinable
     #endif
-    public mutating func append<let count: Int>(_ array: InlineArray<count, UInt8>) {
-        for i in array.indices {
-            let char = array[unchecked: i]
-            if char == 0 {
-                break
-            }
-            self.append(Character(Unicode.Scalar(char)))
-        }
-    }
-
-    #if Inlinable
-    @inlinable
-    #endif
     public func inlineVLArray<E: Error>(_ closure: (consuming VLArray<UInt8>) throws(E) -> Void) rethrows {
         try VLArray<UInt8>.create(string: self, closure)
     }
